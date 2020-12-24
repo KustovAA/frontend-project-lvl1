33356d10a5bb911dd6273abd59c324c502412884
andrey-kustov <andrey-kustov@yandex-team.ru>
2020-12-24 11:47:38 +0300

    Добавляет ci (github actions and eslint check)

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import { askName } from "../src/cli.js";
+import cli from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 
-askName();
+cli();
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,10 @@
 import readlineSync from 'readline-sync';
 
-export const askName = () => {
-	const name = readlineSync.question('May I have your name? ');
+const cli = () => {
+  const
+    name = readlineSync.question('May I have your name? ');
 
-	console.log(`Hello, ${name}`);
-}
+  console.log(`Hello, ${name}`);
+};
+
+export default cli();
